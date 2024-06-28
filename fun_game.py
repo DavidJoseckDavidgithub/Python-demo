48e33726306e0e5aefbe12b5c69b82e56551148d
DavidJoseckDavidgithub <joseckonyinodavid@gmil.com>
2024-06-28 19:05:28 +0300

    Madlib game

---
+++ fun_game.py
@@ -0,0 +1,20 @@
+noun1 = input("Famous_name: ")
+noun2 = input("Famous_city: ")
+University = input("Best_university: ")
+game = input("Python_game: ")
+
+madlib = f"""One sunny morning, {noun1} stumbled upon an ancient manuscript buried deep in the archives of {University} library.
+The manuscript spoke of a legendary code hidden in the Great Python {noun2}, a code so powerful that it could solve any problem in the world. Intrigued, {noun1} decided to embark on a quest to find this legendary code.
+
+Armed with their laptop and an insatiable curiosity, {noun1} set off on their adventure. The first challenge came in the form of a riddle inscribed on the temple’s massive stone doors:
+{noun1} recognized the classic {game} problem and quickly typed out the solution. As the code ran, the stone doors creaked open, revealing the entrance to the {University}.
+
+Inside, {noun1} encountered a series of puzzles, each more complex than the last. They had to use their knowledge of loops, conditionals, and functions to navigate through a maze of code snippets. One puzzle required them to reverse a string, another to sort a list of numbers, and yet another to find the largest prime factor of a number.
+
+Finally, after what felt like hours of coding challenges, {noun1} reached the innermost chamber of the {University} There, on a pedestal, lay a golden USB drive. {noun1} plugged it into their laptop and a message appeared on the screen.
+    {noun1} smiled as they read the message. They realized that the legendary code was not a single line of code, but the knowledge and experience they had gained throughout their journey. With a heart full of wisdom and a mind brimming with ideas, {noun1} returned to Codeville, ready to share their newfound knowledge with fellow coders.
+
+And so, the legend of Python {noun1} and their quest for the ultimate code spread throughout Codeville, inspiring coders young and old to embark on their own adventures in the world of programming."""
+
+print(madlib)
+
